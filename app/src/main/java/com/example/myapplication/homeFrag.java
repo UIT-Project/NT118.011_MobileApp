40a06c419d85be64ac320106ba55e32b9681c3b6
shenkedokato <shenkedokato@gmail.com>
2024-01-04 02:02:24 +0700

    add homefrag

--- app/src/main/java/com/example/myapplication/homeFrag.java
+++ app/src/main/java/com/example/myapplication/homeFrag.java
@@ -2,11 +2,27 @@ package com.example.myapplication;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -14,16 +30,25 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class homeFrag extends Fragment {
+    RecyclerView recyclerView;
+    ArrayList<objectPic> picArrayList;
+    HomeAdapter adapter;
+    FirebaseAuth firebaseAuth;
+    FirebaseDatabase firebaseDatabase;
+    DatabaseReference mDB;
+    FirebaseUser user;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+
+    // TODO: Rename and change types of parameters
+
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
-
     public homeFrag() {
         // Required empty public constructor
     }
@@ -36,7 +61,6 @@ public class homeFrag extends Fragment {
      * @param param2 Parameter 2.
      * @return A new instance of fragment homeFrag.
      */
-    // TODO: Rename and change types and number of parameters
     public static homeFrag newInstance(String param1, String param2) {
         homeFrag fragment = new homeFrag();
         Bundle args = new Bundle();
@@ -50,15 +74,64 @@ public class homeFrag extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+
+            picArrayList=new ArrayList<>();
+
+
+            ArrayList<String> b64Emails= new ArrayList<>();
+            //FB Auth
+            firebaseAuth=FirebaseAuth.getInstance();
+
+            //Kết nối FB Realtime DB
+            firebaseDatabase=FirebaseDatabase.getInstance(
+                    "https://surpic-324b6-default-rtdb.asia-southeast1.firebasedatabase.app");
+            mDB=firebaseDatabase.getReference();
+
+
+            mDB.get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<DataSnapshot> task) {
+                    if(task.isSuccessful()){
+                        for(DataSnapshot childSnapShot: task.getResult().getChildren()){
+                            b64Emails.add(task.getResult().getValue().toString());
+                        }
+                    }
+                }
+            });
+            for(String b64Email:b64Emails){
+                mDB.child(b64Email).child("pics").get().addOnCompleteListener(
+                        new OnCompleteListener<DataSnapshot>() {
+                            @Override
+                            public void onComplete(@NonNull Task<DataSnapshot> task) {
+                                if(task.isSuccessful()){
+                                    for(DataSnapshot childSnapShot : task.getResult().getChildren()){
+                                        picArrayList.add(new objectPic(childSnapShot.getKey(),String.valueOf(childSnapShot.child("data").getValue()),
+                                                String.valueOf(childSnapShot.child("name").getValue()),
+                                                String.valueOf(childSnapShot.child("tags").getValue())));
+                                    }
+
+                                    adapter.notifyDataSetChanged();
+                                }
+                            }
+                        });
+            }
+
         }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view= inflater.inflate(R.layout.fragment_home, container, false);
+//        GeneralFunc.str2Base64()
+        recyclerView=view.findViewById(R.id.recycleview);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new StaggeredGridLayoutManager(2,StaggeredGridLayoutManager.VERTICAL));
+        adapter = new HomeAdapter(picArrayList,view.getContext());
+        recyclerView.setAdapter(adapter);
+
+        return view;
     }
 }
